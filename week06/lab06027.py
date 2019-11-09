a8f1bc76288e2f430e8b93815144700f70f8f493
pjmeehan <G00364732@gmit.ie>
2019-11-09 13:43:11 +0000

    addd new labs week 6

--- week06/lab06027.py
+++ week06/lab06027.py
@@ -2,9 +2,9 @@ import requests
 import json
 
 
-
-apiKey = 'b55d312da577ba479f7dc4f8f3f5b1384bdf3b2e'
-url = 'https://api.github.com/repos/datarepresentationstudent/aPrivateOne'
+# remove the minus sign
+apiKey = 'f59b1cdb743ce0ab9b21a94ac246b02f06ee2d85'
+url = 'https://api.github.com/pjmeehan/dataRepresentation/week02/carviewer2'
 filename ="repo.json"
 
 response = requests.get(url, auth=('token',apiKey))
